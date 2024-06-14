e1453905d0ea75106c5790655da3c008ac6e3e3e
Katerina Kurochkina <katifedr@mail.ru>
2024-06-14 20:35:12 +0500

    widget

--- src/widget.py
+++ src/widget.py
@@ -1,13 +1,13 @@
-import
+import masks
 def mask_account_card(account_card:str) -> str| None:
     """Функция маскировки номера карты или номера счета"""
     numeral = account_card[-20:]
     if numeral.isdigit():
         account = str(numeral)
-        return f"{account_card[0:-20]} {get_mask_account(account)}"
+        return f"{account_card[0:-20]} {masks.get_mask_account(account)}"
     else:
         card_number = str(account_card[-16:])
-        return f"{account_card[0:-16]} {get_mask_card_number(card_number)}"
+        return f"{account_card[0:-16]} {masks.get_mask_card_number(card_number)}"
 
 
 account_card_1 = "Maestro1234567812345678"
