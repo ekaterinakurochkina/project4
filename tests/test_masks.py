35d8353a4fbb5af53223ae3be4d40da3abe54c6a
Katerina Kurochkina <katifedr@mail.ru>
2024-06-20 21:34:22 +0500

    pytest coverage

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,22 +1,23 @@
-from src.masks import get_mask_account, get_mask_card_number
 import pytest
-@pytest.fixture_1
+
+from src.masks import get_mask_account, get_mask_card_number
+
+"""Тестирование раздела masks с использованием фикстуры"""
+
+
+@pytest.fixture
 def account():
     return "01234567890123456789"
 
 
-@pytest.fixture_2
+@pytest.fixture
 def card_number():
     return "1234567812345678"
+
 
 def test_get_mask_account(account):
     assert get_mask_account(account) == "**6789"
 
 
 def test_get_mask_card_number(card_number):
-    assert get_mask_card_number(card_number) == "1234 56** **** 5678 "
-
-# 01234567890123456789,
-#             1122334455667788990011,
-#             abcde,
-#            ""
+    assert get_mask_card_number(card_number) == "1234 56** **** 5678"
