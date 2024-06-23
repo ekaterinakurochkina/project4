6fd146b47b2fa03dbe54099f10110ff4c373ba16
Katerina Kurochkina <katifedr@mail.ru>
2024-06-23 18:40:55 +0500

    generators

--- src/generators.py
+++ src/generators.py
@@ -86,3 +86,14 @@ descriptions = transaction_descriptions(transactions)
 
 for _ in range(5):
     print(next(descriptions))
+
+def card_number_generator(start, end):
+    # генератор номеров банковских карт 9999 9999 9999 9999
+    for i in range (start, end+1):
+        empty_str = "0000000000000000"
+        str_sum = empty_str + str(i)
+        card_number = f"{str_sum[-16:-12]} {str_sum[-12:-8]} {str_sum[-8:-4]} {str_sum[-4:-1]}{str_sum[-1]}"
+        yield card_number
+
+for card_number in card_number_generator(1, 5):
+        print(card_number)
