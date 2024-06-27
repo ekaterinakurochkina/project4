10eaf3c20e3207040a1cf4377365f245dd9f3676
Katerina Kurochkina <katifedr@mail.ru>
2024-06-27 21:57:32 +0500

    11.2 ДЗ с тестированием

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -1,2 +1,13 @@
 import pytest
+from src.decorators import logging, my_function
 
+
+def test_decorator():
+    """Тестирование декоратора"""
+    @logging(filename="mylog.txt")
+    def my_function(x, y):
+        return x + y
+
+
+result = my_function(7, 8)
+assert result == 15
