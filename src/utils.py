674b0b5d17cf2e6bc2178f888088f534201d6687
Katerina Kurochkina <katifedr@mail.ru>
2024-07-07 19:18:54 +0500

    12.1 Разработана функция в модуле Utils и тестирование этой функции в test_utils

--- src/utils.py
+++ src/utils.py
@@ -1,18 +1,19 @@
 from pathlib import Path
 from src.сonfig import ROOT_PATH
-from typing import Dict, Any, List, Set
+from typing import List
 import json
 
 
 def get_transactions(operations: Path) -> List:
     """Функция, возвращающая из json-файла данные о транзакциях"""
     filename = Path(ROOT_PATH, "../data/operations.json")
-    with open(Path(ROOT_PATH,filename)) as json_file:
+    with open(Path(ROOT_PATH, filename)) as json_file:
         try:
             transactions = json.load(json_file)
             return transactions
         except:
             return []
+
 
 # print(get_transactions(Path(ROOT_PATH, "../data/operations.json")))
 # print(type(get_transactions(Path(ROOT_PATH, "../data/operations.json"))))
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,11 +3,33 @@ import json
 from unittest.mock import patch
 from src.utils import get_transactions
 
-@patch('json.load')
-def test_get_transactions(mock_transactions):
-    mock_transactions.return_value = [5, 5]
-    assert get_transactions(mock_transactions) == [5, 5]
-    mock_transactions.assert_called_once_with([5, 5])
+
+def test_get_transactions():
+    with patch ("builtins.open") as mock_open:
+        mock_file = mock_open.return_value.__enter__.return_value
+        mock_file.read.return_value = json.dumps(
+            {
+                "id": 441945886,
+                "state": "EXECUTED",
+                "date": "2019-08-26T10:50:58.294041",
+                "operationAmount": {"amount": "31957.58","currency": {"name": "руб.","code": "RUB"}},
+                "description": "Перевод организации",
+                "from": "Maestro 1596837868705199",
+                "to": "Счет 64686473678894779589",
+            }
+        )
+        assert get_transactions("../data/operations.json")=={
+            "id": 441945886,
+            "state": "EXECUTED",
+            "date": "2019-08-26T10:50:58.294041",
+            "operationAmount": {"amount": "31957.58", "currency": {"name": "руб.", "code": "RUB"}},
+            "description": "Перевод организации",
+            "from": "Maestro 1596837868705199",
+            "to": "Счет 64686473678894779589",
+        }
 
 
 
+    # mock_transactions.return_value = [5, 5]
+    # assert get_transactions(mock_transactions) == [5, 5]
+    # mock_transactions.assert_called_once_with([5, 5])
