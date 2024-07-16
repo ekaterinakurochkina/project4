a380d812e7c4bcef15370defb0749f0837cf679f
Katerina Kurochkina <katifedr@mail.ru>
2024-07-16 10:28:08 +0500

    13.2 Поиск транзакций по описанию, подсчет транзакций с одинаковым описанием - пока не работает функция

--- src/search.py
+++ src/search.py
@@ -2,23 +2,25 @@ import re
 from typing import Dict, List
 from pathlib import Path
 import sys
+from numpy.core.multiarray import item
 sys.path.append(str(Path(__file__).resolve().parent.parent))
 from src.сonfig  import ROOT_PATH
 from src.utils import get_transactions
 from collections import defaultdict
+from collections import Counter
 
-
-def search(transactions: list[Dict], search_data)-> list[Dict]:
-    """Функция поиска транзакции по описанию"""
-    result = []
-    search_data = "Открытие вклада"
-    for transaction in transactions:
-        my_dict = defaultdict(list)
-        if transaction == {}:
-            continue
-        if re.search(search_data, transaction["description"], flags=0):
-            result.append(transaction)
-    return result
+# готовый код!
+# def search(transactions: list[Dict], search_data)-> list[Dict]:
+#     """Функция поиска транзакции по описанию"""
+#     result = []
+#     search_data = "Открытие вклада"
+#     for transaction in transactions:
+#         my_dict = defaultdict(list)
+#         if transaction == {}:
+#             continue
+#         if re.search(search_data, transaction["description"], flags=0):
+#             result.append(transaction)
+#     return result
 
 
 # Проверка кода
@@ -26,28 +28,20 @@ def search(transactions: list[Dict], search_data)-> list[Dict]:
 # transactions = get_transactions(path_to_file)
 # print(search(transactions, ""))
 
+# -----------------------------------
 
 
+def category_search(transactions: list[Dict])-> Dict:
+    """Функция, подсчитывающая кол-во операций в каждой категории"""
+    category_transactions = []
+    category = defaultdict(list)
+    for transaction in transactions:
+        category = transaction["description"]
+        category_transactions.append(category)
+    counted = Counter(category_transactions)
+    return counted
 
-# def category_search(transactions: list[Dict], search_data: List)-> list[Dict]:
-#     """Функция, подсчитывающая кол-во операций в каждой категории"""
-#     result = []
-#     search_data = ""
-#     for transaction in transactions:
-#         print(transaction["description"])
-
-
-
-
-# Определяем функцию для значений по умолчанию
-# def default_value():
-#     return 'неизвестно'
-#
-# # Создаем defaultdict с функцией по умолчанию
-# my_dict = defaultdict(default_value)
-# # Выводим значение для отсутствующего ключа
-# print(my_dict['two'])
-
-
-
-
+# Проверка кода
+path_to_file = Path(ROOT_PATH, "../data/operations.json")
+transactions = get_transactions(path_to_file)
+print(category_search(transactions))
